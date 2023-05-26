78e50c60b22d5ff68b96bb43a942c7e0e3f057cf
YarkanaHome <hakan02@naver.com>
2023-05-27 01:03:31 +0900

    index test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('message', async msg => {
+client.on('message', msg => {
   switch (msg.content) {
     case "ping":
       msg.reply("Pong!");
