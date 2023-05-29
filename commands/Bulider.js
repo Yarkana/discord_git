e6c691a510c31a3d82dace74a9ba3bf896024b66
YarkanaHome <hakan02@naver.com>
2023-05-30 02:00:03 +0900

    db의 변수를 바탕으로 연동하는 시도

--- commands/Bulider.js
+++ commands/Bulider.js
@@ -17,8 +17,10 @@ module.exports = {
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command - 누가 썻는지 알려줍니다
 		// interaction.member is the GuildMember object, which represents the user in the specific guild - 특정 서버 안에?
-		db.SL = 원어;
-		db.TL = 번역어;
+		const SL = interaction.options.getString('원어');
+		const TL = interaction.options.getString('번역어');
+		db.SL = SL;
+		db.TL = TL;
         interaction.reply("원어를 " + db.SL+ "로, 번역어를 " + db.TL+"로 설정했습니다.");
 		
 	},
