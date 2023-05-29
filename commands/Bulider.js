abc6c63d7b20eff66d09d32dc99395d8aaca1344
YarkanaHome <hakan02@naver.com>
2023-05-30 00:59:46 +0900

    슬래쉬커맨드에 필수속성을 부여하고 전역변수를 사용하는 테스트 진행 중

--- commands/Bulider.js
+++ commands/Bulider.js
@@ -1,12 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
+const db = require("../db/db")
 
 module.exports = {
 	data: new SlashCommandBuilder() //이곳에서 명령을 실행함!
 		.setName('언어변경')
-		.setDescription('번역할 언어를 바꿉니다.'),
+		.setDescription('번역할 언어를 바꿉니다.')
+		.addStringOption(option =>
+			option.setName('원어')
+				.setDescription('번역할 원어')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('번역어')
+				.setDescription('번역할 언어')
+				.setRequired(true)),
+		
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command - 누가 썻는지 알려줍니다
 		// interaction.member is the GuildMember object, which represents the user in the specific guild - 특정 서버 안에?
-		await interaction.reply('${interaction.user.username}에 의해 실행되었습니다');
+		db.SL = 원어;
+		db.TL = 번역어;
+		
 	},
 };
---
+++ db/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+	chenalN1: 0,
+	chenalN2: 0,
+	SL: "",
+	TL: "",
+	}
