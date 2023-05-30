4550c0130bdbd029ea6b1cbfe1cb9259d7d12a81
Jihyeon <wlgusqnd7252@naver.com>
2023-05-30 17:47:37 +0900

    프로필 내용 수정

--- events/embedD.js
+++ events/embedD.js
@@ -6,17 +6,16 @@ module.exports = {
 	async execute(message) {     //주내용
   if (message.content == "프로필") {
     const embed = new EmbedBuilder() // 임배드 디스코드 대답
-        //.setAuthor("두두 봇","https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.daegu.ac.kr%2Farticle%2FDG56%2Fdetail%2F516278&psig=AOvVaw1z490kWF08uWED777u4er1&ust=1685287867967000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOCG8tjolf8CFQAAAAAdAAAAABAE")
-    	.setTitle("두두 봇 프로필") // Author은 제목, 이미지 주소 Title은 큰 제목격 
-    	.setURL("https://www.daegu.ac.kr/main") // url 주소 클릭하면 이동됨
+        //.setAuthor("변역 봇","https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.daegu.ac.kr%2Farticle%2FDG56%2Fdetail%2F516278&psig=AOvVaw1z490kWF08uWED777u4er1&ust=1685287867967000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOCG8tjolf8CFQAAAAAdAAAAABAE")
+    	.setTitle("번역 봇 프로필") // Author은 제목, 이미지 주소 Title은 큰 제목격 
+    	.setURL("https://discord.com/api/oauth2/authorize?client_id=1109755131639103508&permissions=8&scope=bot") // url 주소 클릭하면 이동됨
     	.setColor(0xABF200) //색깔 선정
-    	.setDescription("안녕하세요 두두 봇입니다.") // 설명하는 부분, Thumbnail 은 작은 부분
-    	.setThumbnail("https://blog.kakaocdn.net/dn/JCUxA/btqyzvTvIjU/JFStpmCpt4dn8zCQ6vVCx0/img.jpg")
+    	.setDescription("안녕하세요 번역 봇입니다.") // 설명하는 부분, Thumbnail 은 작은 부분
+    	.setThumbnail("https://blog.kakaocdn.net/dn/dFMq6V/btqDJgxK8qF/NadFlTKCzXmAiCCkQSluTk/img.png")
     	.addFields(
-    	  {name: "학교 위치", value: "경상북도 경산시 진량읍 대구대로 201(내리리)",inline: true}, // 이름과 벨륨값을 정해주는 틀 
-    	  {name: "개교일자", value: "1956년 5월 1일(67주년)",inline: true},
-    	  {name: "교훈", value: "큰 뜻을 품어라",inline: true},
-    	  {name: "건학정신", value: "사랑 빛 자유",inline: true}
+    	  {name: "번역어", value: "한국어(ko),영어(en),일본어(ja),중국어간체(zh-CN),중국어번체(zh-TW)",inline: true}, // 이름과 벨륨값을 정해주는 틀 
+    	  {name: "오픈소스", value: "https://github.com/Nestor/discord-translator",inline: true},
+    	  {name: "주소 링크", value: "https://discord.com/api/oauth2/authorize?client_id=1109755131639103508&permissions=8&scope=bot",inline: true}
     )
     
     message.channel.send({ embeds: [embed] });
